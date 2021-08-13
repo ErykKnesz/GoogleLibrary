0b1642dabf93f2ea7e89a500f13bc0a37286de69
Eryk Knesz <eryk.knesz@gmail.com>
2021-08-13 21:05:47 +0200

    add preliminary files config.py, library.py and library package

---
+++ library/models.py
@@ -0,0 +1,12 @@
+from . import db
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    author = db.Column(db.String(80), nullable=False)
+    published_date = db.Column(db.DateTime, nullable=False)
+    ISBN = db.Column(db.Integer, nullable=False)
+    num_pages = db.Column(db.Integer, nullable=False)
+    cover_url = db.Column(db.Text, nullable=False)
+    language = db.Column(db.String(80), nullable=False)
